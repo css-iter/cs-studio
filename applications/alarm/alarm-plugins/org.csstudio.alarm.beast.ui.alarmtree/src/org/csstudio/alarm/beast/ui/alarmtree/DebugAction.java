9ad19a5ff3c27607aca0dd366153fbac7cc4475a
Borut Terpinc <borut.terpinc@cosylab.com>
2018-03-06 23:06:55 +0000

    - Fixed bug where JMS was not properly connected on CS-Studio startup. - Added new virtual/composite alarmClientModel. Needs review and testing.

--- applications/alarm/alarm-plugins/org.csstudio.alarm.beast.ui.alarmtree/src/org/csstudio/alarm/beast/ui/alarmtree/DebugAction.java
+++ applications/alarm/alarm-plugins/org.csstudio.alarm.beast.ui.alarmtree/src/org/csstudio/alarm/beast/ui/alarmtree/DebugAction.java
@@ -7,7 +7,6 @@
  ******************************************************************************/
 package org.csstudio.alarm.beast.ui.alarmtree;
 
-import org.csstudio.alarm.beast.ui.clientmodel.AlarmClientModel;
 import org.eclipse.jface.action.Action;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.swt.widgets.Shell;
@@ -19,20 +18,20 @@ import org.eclipse.swt.widgets.Shell;
 public class DebugAction extends Action
 {
     final private Shell shell;
-    final private AlarmClientModel model;
+    final private ModelProvider modelProvider;
 
-    public DebugAction(final Shell shell, final AlarmClientModel model)
+    public DebugAction(final Shell shell, final ModelProvider modelProvider)
     {
         super("Debug", Activator.getImageDescriptor("icons/debug.gif"));
         setToolTipText("Send debug trigger to alarm server");
         this.shell = shell;
-        this.model = model;
+        this.modelProvider = modelProvider;
     }
 
     @Override
     public void run()
     {
         if (MessageDialog.openConfirm(shell, "Send debug trigger", "Send 'debug' trigger to alarm server?"))
-            model.triggerDebug();
+            modelProvider.getModel().triggerDebug();
     }
 }
