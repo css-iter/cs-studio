2dcc075d9305d89c25abce208c81c5d85daa2251
olme <esteve.olm@gtd.eu>
2019-01-21 09:15:22 +0000

    opibuilder_pvmanager_timeout default value must allow 0

--- applications/opibuilder/opibuilder-plugins/org.csstudio.opibuilder/src/org/csstudio/opibuilder/preferences/OPIRuntimePreferencePage.java
+++ applications/opibuilder/opibuilder-plugins/org.csstudio.opibuilder/src/org/csstudio/opibuilder/preferences/OPIRuntimePreferencePage.java
@@ -140,8 +140,8 @@ public class OPIRuntimePreferencePage extends FieldEditorPreferencePage
                         "PV Connection Layer", entries, parent);
         addField(pvConnectionLayerEditor);
 
-        IntegerFieldEditor pvManagagerTimeout = new IntegerFieldEditor(PreferencesHelper.OPIBUILDER_PVMANAGER_TIMEOUT, "PVManager timeout", parent);
-        pvManagagerTimeout.setValidRange(10, 5000);
+        IntegerFieldEditor pvManagagerTimeout = new IntegerFieldEditor(PreferencesHelper.OPIBUILDER_PVMANAGER_TIMEOUT, "PVManager timeout (ms)", parent);
+        pvManagagerTimeout.setValidRange(0, 5000);
         pvManagagerTimeout.getTextControl(parent).setToolTipText("Timeout to wait (ms) before displaying widgets as disconnected the first time if no values received");
         addField(pvManagagerTimeout);
 
